6a2325759dc2ae289337ea644ee60c36446dbae8
MagicBaconDonuts <ricardoalatorre89@gmail.com>
2023-06-19 23:56:13 -0700

    Adding JWT to store LocalStorage
    Adding usestate and useffect to store a jwt

--- Main/socialmedia-frontend/src/Routes/home/home.component.jsx
+++ Main/socialmedia-frontend/src/Routes/home/home.component.jsx
@@ -1,32 +1,60 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useLocalState } from '../../Components/util/useLocalStorage';
 
 export default function Home() {
-  let navigate = useNavigate();
-  const [user, setUser] = useState({
-    username:"",
-    name:"",
-    lastName:"",
-    password:""
-  })
-  const{username,name,lastName,password} = user
 
-  const onInputChange = (e) => {
-    setUser({...user, [e.target.name]: e.target.value})
-  }
+//   let navigate = useNavigate();
+//   const{username,name,lastName,password} = user
+  
+//   const [user, setUser] = useState({
+//     username:"",
+//     name:"",
+//     lastName:"",
+//     password:""
+//   })
+//   const onInputChange = (e) => {
+//     setUser({...user, [e.target.name]: e.target.value})
+//   }
 
-  const onSubmit = async(e) => {
-    e.preventDefault();
-    await axios.post("create-user", user)
-    navigate("/")
-  }
+  // const onSubmit = async(e) => {
+  //   e.preventDefault();
+  //   await axios.post("create-user", user)
+  //   navigate("/")
+  // }
+
+    const [jwt, setJwt] = useLocalState("", "jwt");
+    useEffect(() => {
+        if(!jwt) {
+            const user = {
+                username: "test",
+                password: "password"
+            };
+          
+            fetch("/api/auth/login", {
+                "headers": {
+                    "Content-Type" : "application/json"
+                },
+                "method": "post",
+                "body": JSON.stringify(user)
+            }).then((response) => Promise.all([response.json(), response.headers]))
+            .then(([body,headers]) => {
+                setJwt(headers.get("authorization"));
+            });
+        }
+    }, []);
+
+    useEffect(() => {
+        console.log(`JWT is ${jwt}`);
+    }, [jwt]);
+
   return (
     <div>
         <div>
             Home/Register
         </div>
-        <form onSubmit={(e) => onSubmit(e)}>
+        {/* <form onSubmit={(e) => onSubmit(e)}>
             <div>
                 <label>UserName</label>
                 <input type='text' placeholder='Enter your Username' name='username' value={username} onChange={(e) => {onInputChange(e)}}/>
@@ -44,7 +72,7 @@ export default function Home() {
                 <input type='password' placeholder='Enter your Password' name='password' value={password} onChange={(e) => {onInputChange(e)}}/>
             </div>
             <button type='submit'>Submit</button>
-        </form>
+        </form> */}
         
     </div>
   )
