f50731a6b851a9f51f7f213a072457d4cd08be6f
MagicBaconDonuts <ricardoalatorre89@gmail.com>
2023-06-20 15:51:20 -0700

    Adding redirection and confirmation to login

--- Main/socialmedia-frontend/src/Components/login/login.component.jsx
+++ Main/socialmedia-frontend/src/Components/login/login.component.jsx
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocalState } from '../../util/useLocalStorage';
 
 const Login = () => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [jwt, setJwt] = useLocalState("", "jwt");
+    console.log(username)
+    console.log(password)
+    function sendLoginRequest(){
+        const user = {
+            username: username,
+            password: password
+        };
+          
+        fetch("/api/auth/login", {
+            "headers": {
+                "Content-Type" : "application/json"
+            },
+            "method": "post",
+            "body": JSON.stringify(user)
+        }).then((response) => {
+            if(response.status === 200)
+                return Promise.all([response.json(), response.headers]);
+            else 
+                return Promise.reject("Invalid login attempt");
+        })
+        .then(([body,headers]) => {
+            setJwt(headers.get("authorization"));
+            window.location.href = "/";
+        }).catch((message) => {
+            alert(message);
+        });
+    }
+
     return (
         <>
             <div>
                 <label htmlFor='username'>Username</label>
-                <input type='text' id='username'/>
+                <input type='text' id='username' value={username} onChange={(e) => {setUsername(e.target.value)}}/>
             </div>
             <div>
                 <label htmlFor='password'>Password</label>
-                <input type='password' id='password'/>
+                <input type='password' id='password' value={password} onChange={(e) => {setPassword(e.target.value)}}/>
+            </div>
+            <div>
+                <button id='submit' type='button'onClick={() => {sendLoginRequest()}} >Submit</button>
             </div>
         </>
     );
--- Main/socialmedia-frontend/src/Routes/home/home.component.jsx
+++ Main/socialmedia-frontend/src/Routes/home/home.component.jsx
@@ -5,45 +5,7 @@ import { useLocalState } from '../../util/useLocalStorage';
 
 export default function Home() {
 
-//   let navigate = useNavigate();
-//   const{username,name,lastName,password} = user
-  
-//   const [user, setUser] = useState({
-//     username:"",
-//     name:"",
-//     lastName:"",
-//     password:""
-//   })
-//   const onInputChange = (e) => {
-//     setUser({...user, [e.target.name]: e.target.value})
-//   }
-
-  // const onSubmit = async(e) => {
-  //   e.preventDefault();
-  //   await axios.post("create-user", user)
-  //   navigate("/")
-  // }
-
     const [jwt, setJwt] = useLocalState("", "jwt");
-    // useEffect(() => {
-    //     if(!jwt) {
-    //         const user = {
-    //             username: "test",
-    //             password: "password"
-    //         };
-          
-    //         fetch("/api/auth/login", {
-    //             "headers": {
-    //                 "Content-Type" : "application/json"
-    //             },
-    //             "method": "post",
-    //             "body": JSON.stringify(user)
-    //         }).then((response) => Promise.all([response.json(), response.headers]))
-    //         .then(([body,headers]) => {
-    //             setJwt(headers.get("authorization"));
-    //         });
-    //     }
-    // }, []);
 
     useEffect(() => {
         console.log(`JWT is ${jwt}`);
@@ -53,27 +15,8 @@ export default function Home() {
     <div>
         <div>
             Home Page
+            test
         </div>
-        {/* <form onSubmit={(e) => onSubmit(e)}>
-            <div>
-                <label>UserName</label>
-                <input type='text' placeholder='Enter your Username' name='username' value={username} onChange={(e) => {onInputChange(e)}}/>
-            </div>
-            <div>
-                <label>Name</label>
-                <input type='text' placeholder='Enter your Name' name='name' value={name} onChange={(e) => {onInputChange(e)}}/>
-            </div>
-            <div>
-                <label>Last Name</label>
-                <input type='text' placeholder='Enter your Last name' name='lastName' value={lastName} onChange={(e) => {onInputChange(e)}}/>
-            </div>
-            <div>
-                <label>Password</label>
-                <input type='password' placeholder='Enter your Password' name='password' value={password} onChange={(e) => {onInputChange(e)}}/>
-            </div>
-            <button type='submit'>Submit</button>
-        </form> */}
-        
     </div>
   )
 }
